9c70b9f33364433024e8b1fcec4ca3a3472602fe
Datan <Datan.ctupide@yandex.ru>
2025-11-24 13:03:50 +0500

    Add files via upload

--- COR_IP/COR_IP.js
+++ COR_IP/COR_IP.js
@@ -3,9 +3,29 @@ function getLocation() {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(
             (position) => {
-                const lat = position.coords.latitude
-                const lon = position.coords.longitude;
-                const mapsLink = `https://www.google.com/maps?q=${lat},${lon}`;
+                let lat = position.coords.latitude
+                let lon = position.coords.longitude;
+                let mapsLink = `https://www.google.com/maps?q=${lat},${lon}`;
+
+                if (lat >= 58 && lon <= 80){
+                    fetch('COR_IP.json')
+                    .then(response => response.json())
+                    .then(data => {
+                    const result = data['KZ'];
+                    result.forEach(item => {
+                    console.log("title:", item.title);
+                    console.log("description:", item.description);
+                    console.log("image:", item.image);
+                    console.log("link:", item.link);
+                    });
+                    })
+                    .catch(error => {
+                        console.error("Ошибка загрузки JSON:", error);
+                    });
+                }
+                else {
+                    console.log(alert)
+                }
 
                 output.innerHTML = `
                     <strong>Ваши координаты:</strong><br>
